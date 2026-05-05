567fec13e967671bf33f121db860963a712e8116
katezavertyaeva <katsiarynazavertyaeva@gmail.com>
2026-05-05 18:08:29 +0200

    add project

---
+++ lesson01/code/console-log-app/index.js
@@ -0,0 +1,9 @@
+console.log("HI");
+
+
+// docker build -t console-log-app .
+// docker run --rm console-log-app
+// docker tag console-log-app katsiarynazavertyaeva/console-log-app
+// docker push katsiarynazavertyaeva/console-log-app
+// docker pull mountains6/console-log-app:latest
+// docker run --rm mountains6/console-log-app:latest
